19296de376db1f491a5c89eca1ae89d0254a5f64
marsim86 <marsim86@gmail.com>
2022-10-27 11:24:22 +0200

    Versión inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.modulo';
+
+import { HeroesModule } from './heroes/heroes.modulo';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Sheldom', 'Thor'];
+  heroeBorrado: string = '';
+
+  
+  borrarHeroe(): void {
+    console.log("borrando ..");
+
+    this.heroeBorrado = this.heroes.shift() || '';
+    this.heroeBorrado.length
+  }
+}
